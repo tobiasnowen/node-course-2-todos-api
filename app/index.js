6ace2e438e1424b4666bc3a8d3e71419f0198dd0
tobiasnowen <tobiasnowen@gmail.com>
2017-06-22 13:28:52 +0200

    first route added.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,22 @@
-console.log('ok');
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db');
+const {Todo, User} = require('./models');
+// const Todo = require('./models/todo');
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    new Todo({
+        text: req.body.text
+    }).save()
+        .then(doc => res.status(200).json(doc))
+        .catch(er => res.status(400).json({message: er.message}));
+});
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
---
+++ app/models/index.js
@@ -0,0 +1,3 @@
+module.exports = Object.assign({},
+    require('./todo'),
+    require('./user'));
